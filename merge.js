91b615f2d2f728e5957190bee289a0c510840538
JKirchartz <jkirchartz@gmail.com>
2018-11-10 22:37:17 -0500

    Fix a big bug in how sentences are tagged.
    Previously, not every instance of a word in a sentence was replaced with a POS -
    this change ensures that every word is considered and properly replaced
    with a reference to a particular POS.
    
    Also, replace tabs with spaces - sorry if you hate it!

--- merge.js
+++ merge.js
@@ -8,43 +8,43 @@ let input_B = process.argv[3] || null;
 let output = process.argv[4] || null;
 
 if (input_A, input_B) {
-	let output_json = JSON.parse(fs.readFileSync(input_A).toString());
-	let file_B = JSON.parse(fs.readFileSync(input_B).toString());
+  let output_json = JSON.parse(fs.readFileSync(input_A).toString());
+  let file_B = JSON.parse(fs.readFileSync(input_B).toString());
 
-	for (let key in file_B) {
-		console.log('has %s? %s', key, typeof output_json[key] !== 'undefined');
-		if (typeof output_json[key] !== 'undefined') {
-			console.log('concatenating arrays');
-			output_json[key] = output_json[key].concat(file_B[key]);
-			output_json[key] = Array.from(new Set(output_json[key]));
-		} else {
-			console.log('setting solitary array');
-			output_json[key] = file_B[key];
-		}
-	}
+  for (let key in file_B) {
+    console.log('has %s? %s', key, typeof output_json[key] !== 'undefined');
+    if (typeof output_json[key] !== 'undefined') {
+      console.log('concatenating arrays');
+      output_json[key] = output_json[key].concat(file_B[key]);
+      output_json[key] = Array.from(new Set(output_json[key]));
+    } else {
+      console.log('setting solitary array');
+      output_json[key] = file_B[key];
+    }
+  }
 
-	if (output_json) {
-		if (output) {
-			output_json = JSON.stringify(output_json).replace(new RegExp('","', 'g'), '",\n    "').replace(new RegExp('],"', 'g'),'],\n"');
+  if (output_json) {
+    if (output) {
+      output_json = JSON.stringify(output_json).replace(new RegExp('","', 'g'), '",\n    "').replace(new RegExp('],"', 'g'),'],\n"');
 
-			fs.writeFile(output,
-				output_json,
-				function (err) {
-					if (err) {return console.log('everything sucks because: ', err);}
-					console.log('wrote grammar to %s', output);
-					process.exit(0);
-				});
-		} else {
-			console.log(output_json);
-			process.exit(0);
-		}
-	} else {
-		console.log("something went wrong merging files");
-		process.exit(1);
-	}
+      fs.writeFile(output,
+        output_json,
+        function (err) {
+          if (err) {return console.log('everything sucks because: ', err);}
+          console.log('wrote grammar to %s', output);
+          process.exit(0);
+        });
+    } else {
+      console.log(output_json);
+      process.exit(0);
+    }
+  } else {
+    console.log("something went wrong merging files");
+    process.exit(1);
+  }
 
 } else {
-	console.error('please specify 2 input files');
-	process.exit(1);
+  console.error('please specify 2 input files');
+  process.exit(1);
 }
 
