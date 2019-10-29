96cf4f9205185b8fc534aeac9c18ad4292cf9d8d
JKirchartz <jkirchartz@gmail.com>
2019-10-29 18:53:47 -0400

    finish converting to modules/cli tool (I think)

--- bin/pos2tracery.js
+++ bin/pos2tracery.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+'use strict';
+
 /*
  * pos2tracery.js
  * Copyleft (ↄ) 2019 jkirchartz <me@jkirchartz.com>
@@ -8,11 +11,11 @@
 var yargs = require('yargs');
 
 yargs.version()
-  .command(["$0", "pos", "p"], "convert corpus to tracery grammar based on parts-of-speech", require('../lib/pos2tracery.js'))
-  .command(["merge", "m"], "merge 2 tracery grammars", require('../lib/merge.js'))
-  .command(["generate", "gen", "g"], "generate text from tracery grammar", require('../lib/tracery.js'))
-  .help('h').alias('h', 'help').argv;
-
-// TODO: use handlers to send correct arguments to module.exports functions
-
-
+  .usage('Usage: $0 [pos|merge|generate|completion]')
+  .commandDir('../lib')
+  .completion('completion')
+  .demandCommand()
+  .wrap(100)
+  .help('h')
+  .alias('h', 'help')
+  .argv;
--- index.js
+++ index.js
@@ -7,7 +7,31 @@
 
 
 module.exports = {
-  "pos2tracery": require('lib/pos2tracery'),
-  "merge": require('lib/merge'),
-  "tracery": require('lib/tracery')
+  "pos2tracery": function pos2tracery(input, output, percent, modifiers, origin, ignore, verbose) {
+    require('lib/pos2tracery').handler({
+      input: input,
+      output: output,
+      percent: percent || 100,
+      modifiers: modifiers || false,
+      origin: origin || true,
+      ignore: ignore,
+      verbose: verbose
+    });
+  },
+  "merge": function merge(inputA, inputB, output, verbose) {
+    require('lib/merge').handler({
+      inputA: inputA,
+      inputB: inputB,
+      output: output,
+      verbose: verbose
+    });
+  },
+  "generate": function generate(grammar, modifiers, origin, repeat) {
+    require('lib/tracery').handler({
+      grammar: grammar,
+      modifiers: modifiers || true,
+      origin: origin || "origin",
+      repeat: repeat || 1
+    });
+  }
 };
